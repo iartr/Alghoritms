620b049aad9439bad36f71a09e6be5a41b6f5652
a.ilkaev <a.ilkaev@corp.vk.com>
2022-06-22 23:29:44 +0500

    climb-stairs

--- src/main/kotlin/leetcode/DynamicClimbStairs.kt
+++ src/main/kotlin/leetcode/DynamicClimbStairs.kt
@@ -1,24 +1,47 @@
 package leetcode
 
 // https://leetcode.com/problems/climbing-stairs/
+// https://youtu.be/Y0lT9Fck7qI
+
 fun main() {
-    println(climbStairs(5)) // 10
-    println(climbStairs(4)) // 6
-    println(climbStairs(3)) // 3
-    println(climbStairs(1)) // 1
+    println(climbStairs(37))
+    println(climbStairs(5))
+    println(climbStairs(4))
+    println(climbStairs(3))
+    println(climbStairs(1))
 }
 
-fun climbStairs(n: Int): Int {
-    var res = 0
+val cache = mutableMapOf<Int, Int>(
+    0 to 1,
+    1 to 1,
+    2 to 2
+)
 
-    for (i in 0 until n) {
-        if (i + 1 <= n) {
-            res++
-        }
-        if (i + 2 <= n) {
-            res++
-        }
+// Fibonacci O(n)
+fun climbStairs(n: Int): Int {
+    if (cache.containsKey(n)) {
+        return cache.getValue(n)
     }
 
-    return res
+    val value = climbStairs(n - 1) + climbStairs(n - 2)
+    cache[n] = value
+    return value
+}
+
+fun fibonacciBestSolution(n: Int): Int {
+    // base cases
+    if (n <= 0) return 0
+    if (n == 1) return 1
+    if (n == 2) return 2
+
+    var oneStepBefore = 2
+    var twoStepsBefore = 1
+    var allWays = 0
+
+    for (i in 2 until n) {
+        allWays = oneStepBefore + twoStepsBefore
+        twoStepsBefore = oneStepBefore
+        oneStepBefore = allWays
+    }
+    return allWays
 }
